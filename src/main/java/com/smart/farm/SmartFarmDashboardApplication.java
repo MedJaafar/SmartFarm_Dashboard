4eb6b99c5794fc429cbdf2baeba93f0f18a465d0
medja3 <jaafarmohamed103@gmail.com>
2021-05-16 21:29:29 +0100

    Update for a new Sprint

--- src/main/java/com/smart/farm/SmartFarmDashboardApplication.java
+++ src/main/java/com/smart/farm/SmartFarmDashboardApplication.java
@@ -19,8 +19,7 @@ public class SmartFarmDashboardApplication {
 	@Bean
     public MongoTemplate mongoTemplate(MongoDbFactory mongoDbFactory, MongoMappingContext context) {
         MappingMongoConverter converter = new MappingMongoConverter(new DefaultDbRefResolver(mongoDbFactory), context);
-        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
-        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory, converter);
-        return mongoTemplate;
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null)); 
+        return new MongoTemplate(mongoDbFactory, converter);
   }
 }
